2f348362f6a13395ec67e3fd17a511853c5f8d9e
mutsuyuki <muttuwo.mob@gmail.com>
2020-04-13 22:49:18 +0900

    地球儀のサンプル作成

---
+++ Assets/_Particle/Earth/EarthAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class EarthAnimation : MonoBehaviour {
+    void Update() {
+        transform.Rotate(Vector3.up, 0.5f);
+        float rate = ((float) Math.Sin(Time.time * 0.8f) + 1f) / 2f;
+        GetComponent<VisualEffect>().SetFloat("SphereRate", rate);
+    }
+}
