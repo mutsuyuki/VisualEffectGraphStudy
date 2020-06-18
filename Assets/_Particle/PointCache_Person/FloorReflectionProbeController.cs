616bdcd888c50c5a4d64e86d9aba6b3fe601390e
mutsuyuki <muttuwo.mob@gmail.com>
2020-06-18 22:38:11 +0900

    Update Person

---
+++ Assets/_Particle/PointCache_Person/FloorReflectionProbeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorReflectionProbeController : MonoBehaviour
+{
+    [SerializeField] Transform floor;
+    ReflectionProbe probe;
+    
+    void Start() {
+        this.probe = GetComponent<ReflectionProbe>();
+    }
+    
+    void Update () {
+        this.probe.transform.position = new Vector3(
+            Camera.main.transform.position.x, 
+            floor.position.y - ( Camera.main.transform.position.y - floor.position.y), 
+            Camera.main.transform.position.z
+        );
+
+        probe.RenderProbe();
+    }
+}
